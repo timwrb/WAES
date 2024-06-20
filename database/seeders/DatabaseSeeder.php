0fd4c5de4a847e724d64d8f9512006f23474db4a
timwrb <wurmbrand.tim@gmail.com>
2024-06-20 15:56:09 +0200

    .implemented seeder and factories for Category and Expense

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-
         $this->call([
             CategorySeeder::class,
             ExpenseSeeder::class,
